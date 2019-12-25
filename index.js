e457daf81e7f41e75bfbc39fc0438e11dd71a20f
Michael Luo <shaochuanl@student.unimelb.edu.au>
2019-12-25 21:37:09 +1100

    Complete a simple proj

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 // read file following the absolute path
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); 
@@ -7,11 +8,64 @@ const laptopData = JSON.parse(json);
 
 // create a server and each time someone get access my server
 const server = http.createServer((req, res) => {
-    // http status code + content type
-    res.writeHead(404, {'Content-type': 'text/html'});
-    res.end('This is the response!');
+
+    // true is for creating an object
+    const pathName = url.parse(req.url, true).pathname;
+    console.log(pathName);
+    const id = url.parse(req.url, true).query.id;
+
+    // PRODUCTS OVERVIEW
+    if (pathName === '/products' || pathName === '/' ) {
+        // http status code + content type
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                res.end(overviewOutput);
+            });
+        });
+        
+    }
+    // IMAGES
+    else if((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, {'Content-type': 'image/jpg'});
+            res.end(data);
+        }); 
+    }
+    // LAPTOP DETAIL
+    else if(pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        });
+    }
+    // URL NOT FOUND
+    else {
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.end('URL was not gound on the server!');
+    }
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for request now');
 });
+
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
